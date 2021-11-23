03b054d85f91059192ca0efe50fbd51a084dc11b
kaurlove12345 <lovepreet@nimbletechnocrats.com>
2021-11-23 16:45:40 +0530

    database updated

--- database/migrations/2021_11_23_111306_update_users_table.php
+++ database/migrations/2021_11_23_111306_update_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCategoryNameToProductsTable extends Migration
+class UpdateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddCategoryNameToProductsTable extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('category_name')->after('collection_name');
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('flag')->after('id');
+            $table->string('membership')->after('email');
+            $table->string('image')->after('membership')->nullable();
         });
     }
 
@@ -25,7 +27,7 @@ class AddCategoryNameToProductsTable extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
